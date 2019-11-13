71cd702a74ef83131ca612fe72a51aa8a4fc53fb
Sujata Swamy <sujata.swamy@airfi.aero>
2019-11-13 22:12:00 +0530

    sujata has done modifications

--- app/src/main/java/demo/android/com/myapplication/Mainactivity.java
+++ app/src/main/java/demo/android/com/myapplication/Mainactivity.java
@@ -3,6 +3,7 @@ package demo.android.com.myapplication;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class Mainactivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class Mainactivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("","");
     }
 }
